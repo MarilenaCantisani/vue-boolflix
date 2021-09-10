eb5c1c15ebdf16876239bef0a7e24e419d84f802
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-10 17:33:08 +0200

    Addition call axios to tv series

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,29 @@
     </header>
     <!--//// MAIN -->
     <main>
-      <!-- Search results movies -->
-      <ul v-for="movie in movies" :key="movie.id">
-        <li>Titolo: {{ movie.title }}</li>
-        <li>Titolo Originale: {{ movie.original_title }}</li>
-        <li>Lingua: {{ movie.original_language }}</li>
-        <li>Voto: {{ movie.vote_average }}</li>
-      </ul>
+      <ResultList :list="movies" :type="typeMovie" />
+      <hr />
+      <ResultList :list="series" :type="typeSeries" />
     </main>
   </div>
 </template>
 
 <script>
 import Search from "@/components/Search.vue";
+import ResultList from "@/components/ResultList.vue";
 import axios from "axios";
 export default {
   name: "App",
   components: {
     Search,
+    ResultList,
   },
   data() {
     return {
+      typeMovie: "movies",
+      typeSeries: "series",
       movies: [],
+      series: [],
       api: {
         baseUrl: "https://api.themoviedb.org/3",
         key: "65c487d37adb6d43703803bbf76ecede",
@@ -43,6 +44,14 @@ export default {
         .then((res) => {
           console.log(res.data.results);
           this.movies = res.data.results;
+        });
+      axios
+        .get(
+          `${this.api.baseUrl}/search/tv?api_key=${this.api.key}&query=${term}`
+        )
+        .then((res) => {
+          console.log(res.data.results);
+          this.series = res.data.results;
         });
     },
   },
---
+++ src/components/ResultList.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="search-result">
+    <!-- Search results movies-->
+    <div v-if="type === 'movies'">
+      <ul v-for="item in list" :key="item.id">
+        <li>Titolo: {{ item.title }}</li>
+        <li>Titolo Originale: {{ item.original_title }}</li>
+        <li v-if="languageFlag.includes(item.original_language)">
+          Lingua:
+          <img
+            :src="language(item.original_language)"
+            alt="item.original_language"
+            width="20"
+            height="12"
+          />
+        </li>
+        <li v-else>Lingua: {{ item.original_language }}</li>
+        <li>Voto: {{ item.vote_average }}</li>
+      </ul>
+    </div>
+
+    <!-- Search results movies-->
+    <div v-else>
+      <ul v-for="item in list" :key="item.id">
+        <li>Titolo: {{ item.name }}</li>
+        <li>Titolo Originale: {{ item.original_name }}</li>
+        <li v-if="languageFlag.includes(item.original_language)">
+          Lingua:
+          <img
+            :src="language(item.original_language)"
+            alt="item.original_language"
+            width="20"
+            height="12"
+          />
+        </li>
+        <li v-else>Lingua: {{ item.original_language }}</li>
+        <li>Voto: {{ item.vote_average }}</li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ResultList",
+  props: ["list", "type"],
+  data() {
+    return {
+      languageFlag: ["it", "en"],
+    };
+  },
+  methods: {
+    language(text) {
+      return require(`../assets/${text}.png`);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+
